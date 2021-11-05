caca3b5c989be150761b7b7c6ae89dc8a2478060
Ian <DrilonIan@yahoo.com>
2021-11-05 23:43:58 +0800

    adding illustrations for disease detection

--- client/src/components/Tooltip/Tooltip.jsx
+++ client/src/components/Tooltip/Tooltip.jsx
@@ -4,8 +4,12 @@ import "./tooltip.scss"
 export default function Tooltip() {
     return (
         <div className="tooltip-wrapper">
-            <motion.div className="content" initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: 5 }} exit={{ opacity: 0, y: 0 }}>
-                Tooltip
+            <motion.div className="tooltip-card" initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: 5 }} exit={{ opacity: 0, y: 0 }}>
+                <div className="content">
+                    <p>The detection reach 95% for <strong>Cancer</strong>,
+                        you might need to consider visiting the doctor for further check up</p>
+                    <img src="./images/illustrations/Sad.svg" alt="" />
+                </div>
             </motion.div>
         </div>
     )
