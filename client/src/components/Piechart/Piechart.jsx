0df7eaacc359790c392315ccad4484433541df8e
Ian <DrilonIan@yahoo.com>
2021-11-05 22:35:56 +0800

    deleting trash

--- client/src/components/Piechart/Piechart.jsx
+++ client/src/components/Piechart/Piechart.jsx
@@ -1,28 +1,39 @@
-import { PieChart, Pie, Cell, Label } from 'recharts'
-import "./piechart.scss"
+import { PieChart, Pie, Cell, Label } from "recharts";
+import "./piechart.scss";
+
+const ATTRIBUTES = {
+    color: "#7a80a0",
+    position: "middle",
+    size: "15px",
+    dy: 5,
+};
 
 export default function Piechart() {
-
     const CustomLabel = ({ viewBox, threshold = 0, label }) => {
         const { cx, cy } = viewBox;
         return (
             <g>
-                <text x={cx} y={cy - 10} dy={5} textAnchor="middle">
-                    <tspan
-                        style={{
-                            fontSize: "15px",
-                            fontWeight: 700,
-                            color: "#d7ddec"
-                        }}
-                    >
-                        {threshold}
-                    </tspan>
+                <text
+                    fill={ATTRIBUTES.color}
+                    x={cx}
+                    y={cy - 10}
+                    dy={ATTRIBUTES.dy}
+                    textAnchor={ATTRIBUTES.position}
+                    fontSize={ATTRIBUTES.size}
+                >
+                    <tspan>{threshold}</tspan>
                 </text>
-                <text x={cx} y={cy + 8} dy={5} textAnchor="middle">
+                <text
+                    fill={ATTRIBUTES.color}
+                    x={cx}
+                    y={cy + 8}
+                    dy={ATTRIBUTES.dy}
+                    textAnchor={ATTRIBUTES.position}
+                    fontSize={ATTRIBUTES.size}
+                >
                     <tspan
                         style={{
                             fontSize: "15px",
-                            color: "#A6ADD1"
                         }}
                     >
                         {label}
@@ -34,26 +45,33 @@ export default function Piechart() {
 
     const data01 = [
         {
-            "name": "Group A",
-            "value": 400
+            name: "Group A",
+            value: 400,
         },
         {
-            "name": "Group B",
-            "value": 1000
-        }
-
+            name: "Group B",
+            value: 1000,
+        },
     ];
 
     return (
         <PieChart width={220} height={150} className="pie-wrapper">
-            <Pie data={data01} dataKey="value" nameKey="name" cx="70%" cy={"50%"} innerRadius={55} outerRadius={65} fill="#8884d8" >
+            <Pie
+                data={data01}
+                dataKey="value"
+                nameKey="name"
+                cx="70%"
+                cy="50%"
+                innerRadius={55}
+                outerRadius={65}
+                fill="#8884d8"
+            >
                 <Label
-                    content={<CustomLabel threshold={"50%"} label={"Liver Corrisis"} />}
-
+                    content={<CustomLabel threshold={"100%"} label={"Liver Corrisis"} />}
                 />
                 <Cell fill="#2C3855" />
                 <Cell fill="#E1F1FF" />
             </Pie>
         </PieChart>
-    )
+    );
 }
