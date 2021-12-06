b4a88a0d12fb5bce00fbfc215ac94889bc1fbb15
Ian <DrilonIan@yahoo.com>
2021-12-07 00:03:33 +0800

    system esign

--- flask-server/server.py
+++ flask-server/server.py
@@ -9,9 +9,9 @@ def receiveImages():
     return request.json
 
 
-# @app.route("/members")
-# def members():
-#     return {"members": ["Member 1", "Member 2", "Member 2"]}
+@app.route("/members")
+def members():
+    return {"members": ["Member 1", "Member 2", "Member 2"]}
 
 
 if __name__ == "__main__":
